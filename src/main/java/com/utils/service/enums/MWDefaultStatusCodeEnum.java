e273a10e0f9e46662b5907de2fd5358b8b880f77
Moataz.ElSawy <Moataz.ElSawy@BVS-Workstation.eg.Albaraka.local>
2022-07-05 12:51:47 +0200

    Middlware error mapping

--- src/main/java/com/utils/service/enums/MWDefaultStatusCodeEnum.java
+++ src/main/java/com/utils/service/enums/MWDefaultStatusCodeEnum.java
@@ -1,32 +1,26 @@
 package com.utils.service.enums;
 
-
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @ToString
 public enum MWDefaultStatusCodeEnum {
+	ATOMIC_SUCCESS("MW_0000"), SUCCESS("0000"), GENERAL_ERROR("-9999"), BE_SOAP_NOT_AVAILABLE("-6666"),
+	BE_REST_NOT_AVAILABLE("-7777"), INVALID_MOBILE_NUMBER("AT_UTIL_2"), EMPTY_BODY_OR_MOBILE_NO("AT_UTIL_3");
 
-    SUCCESS("AT_UTIL_1"),
-    INVALID_MOBILE_NUMBER("AT_UTIL_2"),
-    EMPTY_BODY_OR_MOBILE_NO("AT_UTIL_3"),
-    GENERAL_ERROR ("AT_UTIL_4"),
-    ;
+	private final String code;
 
+	MWDefaultStatusCodeEnum(String code) {
+		this.code = code;
+	}
 
-    private final String code;
-
-    MWDefaultStatusCodeEnum(String code) {
-        this.code = code;
-    }
-
-    public static MWDefaultStatusCodeEnum fromId(String id) {
-        for (MWDefaultStatusCodeEnum at : MWDefaultStatusCodeEnum.values()) {
-            if (at.getCode().equals(id)) {
-                return at;
-            }
-        }
-        return null;
-    }
+	public static MWDefaultStatusCodeEnum fromId(String id) {
+		for (MWDefaultStatusCodeEnum at : MWDefaultStatusCodeEnum.values()) {
+			if (at.getCode().equals(id)) {
+				return at;
+			}
+		}
+		return null;
+	}
 }
